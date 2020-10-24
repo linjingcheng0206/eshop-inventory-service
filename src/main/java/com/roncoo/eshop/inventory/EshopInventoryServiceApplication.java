6d9ce8f7daa2be18ca89a600cb1b88028172e597
ljc <linjingcheng0206@163.com>
2020-10-24 18:36:59 +0800

    first commit

---
+++ src/main/java/com/roncoo/eshop/inventory/EshopInventoryServiceApplication.java
@@ -0,0 +1,28 @@
+package com.roncoo.eshop.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EshopInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopInventoryServiceApplication.class, args); 
+	}
+
+	@Bean
+	public JedisPool jedisPool() {
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxTotal(100);
+		config.setMaxIdle(5);
+		config.setMaxWaitMillis(1000 * 10);
+		config.setTestOnBorrow(true);
+//		return new JedisPool(config, "localhost", 6379);
+		return new JedisPool(config, "192.168.1.16", 1111);//访问redis主集群
+	}
+}
